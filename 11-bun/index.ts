a96fc1606762913385c17901fe57844266c492b0
Stroopwafe1 <48443491+Stroopwafe1@users.noreply.github.com>
2024-12-11 09:45:12 +0100

    Chore: Cleaner Day 11 solution

--- 11-bun/index.ts
+++ 11-bun/index.ts
@@ -5,69 +5,55 @@ if (process.argv.length === 2) {
 const file = Bun.file(process.argv[2]);
 const content = (await file.text()).trim();
 
-let arr1 = content.split(' ').map(e => Number.parseInt(e));
-const N = 25;
-
-for (let i = 0; i < N; i++) {
-	arr1 = arr1.flatMap(e => {
-		if (e === 0) return 1;
-		const str = e.toString();
-		if (str.length % 2 === 0) {
-			const middle = Math.floor(str.length / 2);
-			return [
-				Number.parseInt(str.substring(0, middle)),
-				Number.parseInt(str.substring(middle))
-			];
-		}
-		return e * 2024;
-	});
-}
-
-console.log('Solution part 1: ', arr1.length);
-
-let obj = {};
-let nextObj = {};
-content.split(' ').forEach(e => {
-	obj[e] = 1;
-});
-const N2 = 75;
-
-const incrementKey = (key: string, count: number) => {
+const incrementKey = (obj: object, nextObj: object, key: string, count: number) => {
 	if (key in nextObj) {
 		nextObj[key] += count;
 	} else {
 		nextObj[key] = (key in obj) ? obj[key] + count : count;
 	}
 }
-const decrementKey = (key: string, count: number) => {
+const decrementKey = (obj: object, nextObj: object, key: string, count: number) => {
 	if (key in nextObj)
 		nextObj[key] -= count;
 	else
 		nextObj[key] = obj[key] - count;
 }
 
-for (let i = 0; i < N2; i++) {
-	for (let key in obj) {
-		const count = obj[key];
-		decrementKey(key, count);
-		if (key === '0') {
-			incrementKey('1', count);
-		} else if (key.length % 2 === 0) {
-			const middle = Math.floor(key.length / 2);
-			let key1 = Number.parseInt(key.substring(0, middle)).toString();
-			incrementKey(key1, count);
-			key1 = Number.parseInt(key.substring(middle)).toString();
-			incrementKey(key1, count);
-		} else {
-			incrementKey((Number.parseInt(key) * 2024).toString(), count);
-		}
+const solution = (part: number): number => {
+	let obj = {};
+	let nextObj = {};
+	content.split(' ').forEach(e => {
+		obj[e] = 1;
+	});
 
+	const N = part === 1 ? 25 : 75;
+	let sum = 0;
+	for (let i = 0; i < N; i++) {
+		for (let key in obj) {
+			const count = obj[key];
+			decrementKey(obj, nextObj, key, count);
+			if (key === '0') {
+				incrementKey(obj, nextObj, '1', count);
+			} else if (key.length % 2 === 0) {
+				const middle = Math.floor(key.length / 2);
+				let key1 = Number.parseInt(key.substring(0, middle)).toString();
+				incrementKey(obj, nextObj, key1, count);
+				key1 = Number.parseInt(key.substring(middle)).toString();
+				incrementKey(obj, nextObj, key1, count);
+			} else {
+				incrementKey(obj, nextObj, (Number.parseInt(key) * 2024).toString(), count);
+			}
+
+		}
+		obj = nextObj;
+		nextObj = {};
 	}
-	obj = nextObj;
-	nextObj = {};
+
+	for (let key in obj) {
+		sum += obj[key];
+	}
+	return sum;
 }
-let sum = 0;
-for (let key in obj) {
-	sum += obj[key];
-}
-console.log('Solution part 2: ', sum);
+
+console.log('Solution part 1: ', solution(1));
+console.log('Solution part 2: ', solution(2));
